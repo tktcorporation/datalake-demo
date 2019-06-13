4fefd9bd30bc99e5f41a4b4deb750534d78d2431
samashtar <sam.ashtar@flatironschool.com>
2019-06-13 10:54:06 -0400

    using the data on the horizontal bar graph

--- src/components/partials/charts/HorizontalBar.js
+++ src/components/partials/charts/HorizontalBar.js
@@ -1,4 +1,6 @@
 import { HorizontalBar } from 'vue-chartjs';
+const ColorScheme = require('color-scheme');
+import moment from 'moment';
 
 export default {
     extends: HorizontalBar,
@@ -38,58 +40,71 @@ export default {
     mounted() {
         this.render();
     },
+    computed: {
+        tagData() {
+            return this.$store.getters.tagData;
+        }
+    },
     methods: {
         render() {
             //labels = tag names
             // datasets  = //
             ///////label = usagmnetwork
             ///////data = interactions
-            this.renderChart(
-                {
-                    labels: [
-                        'January',
-                        'February',
-                        'March',
-                        'April',
-                        'May',
-                        'June',
-                        'July',
-                        'August',
-                        'September',
-                        'October',
-                        'November',
-                        'December'
-                    ],
-                    datasets: [
-                        {
-                            type: 'horizontalBar',
-                            label: 'Invoiced',
-                            backgroundColor: '#12c44c',
-                            data: [
-                                1050,
-                                900,
-                                1000,
-                                850,
-                                820,
-                                420,
-                                700,
-                                1010,
-                                999,
-                                340,
-                                0,
-                                0
-                            ]
-                        },
-                        {
-                            type: 'horizontalBar',
-                            label: 'Order',
-                            backgroundColor: 'red',
-                            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 800, 120, 90]
-                        }
-                    ]
-                },
-                this.options
-            );
+            this.chartData = {
+                datasets: [],
+                labels: []
+            };
+
+            //only creates 12 colors right now
+            let scheme = new ColorScheme();
+            scheme
+                .from_hue(21)
+                .scheme('triade')
+                .variation('hard');
+
+            let colors = scheme.colors();
+            // difference between all graphs
+            this.tagData.forEach((tag, index, array) => {
+                this.chartData.labels = [...this.chartData.labels, tag.name];
+                let color = colors[index];
+                // push objects into the datasets
+                if (
+                    this.chartData.datasets.filter(
+                        dataset => dataset.label === tag.profile.usagm_network
+                    ).length > 0
+                ) {
+                    let currentDataset = this.chartData.datasets.filter(
+                        dataset => dataset.label === tag.profile.usagm_network
+                    )[0];
+                    let newDataPoint = {
+                        x: tag.interactions,
+                        y: tag.name
+                    };
+                    currentDataset.data = [
+                        ...currentDataset.data,
+                        newDataPoint
+                    ];
+                } else {
+                    let newDataSet = {
+                        type: 'horizontalBar',
+                        label: tag.profile.usagm_network,
+                        backgroundColor: `#${color}`,
+                        data: [
+                            {
+                                x: tag.interactions,
+                                y: tag.name
+                            }
+                        ]
+                    };
+                    this.chartData.datasets = [
+                        ...this.chartData.datasets,
+                        newDataSet
+                    ];
+                }
+            });
+
+            this.renderChart(this.chartData, this.options);
         }
     }
 };
